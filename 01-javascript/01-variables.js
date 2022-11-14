8732dc6b59132eaec4afe25179391ea6bdbff1a1
Cesar J. Santacruz <cesarjsantacruz2000@gmail.com>
2022-11-14 17:58:29 -0500

    [IMP][CS] Variables primitivas

--- 01-javascript/01-variables.js
+++ 01-javascript/01-variables.js
@@ -13,3 +13,26 @@ numeroDos = true;
 const configuracionArchivos = 'PDF';
 // configuracionArchivos = 'XML';
 // Vamos a preferir CONST > LET > NUNCA VAR!
+
+// Variables primitivas
+const numero = 1; //number
+const sueldo = 1.2; //number
+const texto = "Cesar"; //string
+const apellidos = 'Santacruz'; //string
+const booleano = true; //boolean
+const hijos = null; //object
+const zapatos = undefined; //undefined
+
+console.log(typeof numero);
+console.log(typeof sueldo);
+console.log(typeof texto);
+console.log(typeof apellidos);
+console.log(typeof booleano);
+console.log(typeof hijos);
+console.log(typeof zapatos);
+
+// Repo path
+// cd E:\GitKraken\santacruz-portilla-cesar-joel-web-adv\
+// cd 01-javascript
+// Run command
+// node 01-variables.js
