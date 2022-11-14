900664b24d9654ccb1d54aa32cd1bb5c6496aa92
Cesar J. Santacruz <cesarjsantacruz2000@gmail.com>
2022-11-14 17:51:03 -0500

    [IMP][CS] Mutable and immutable variables

--- 01-javascript/01-variables.js
+++ 01-javascript/01-variables.js
@@ -1,2 +1,15 @@
 // /01-javascript
 //      01-variables.js
+
+// mutables (re asignadas)
+var numeroUno = 1;
+let numeroDos = 2;
+numeroUno = 12;
+numeroDos = 8;
+numeroUno = false;
+numeroDos = true;
+
+// Inmutables
+const configuracionArchivos = 'PDF';
+// configuracionArchivos = 'XML';
+// Vamos a preferir CONST > LET > NUNCA VAR!
