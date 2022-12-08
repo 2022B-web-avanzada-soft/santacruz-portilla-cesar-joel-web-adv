79bc3b8871145785467a0d72953183d14ca39c46
Cesar J. Santacruz <cesarjsantacruz2000@gmail.com>
2022-12-08 17:35:07 -0500

    [IMP][CS] Promesas parte teorica

---
+++ 01-javascript/07-promesas.js
@@ -0,0 +1,46 @@
+// /07-promesas
+//      07-promesas.js
+
+function promesaEsPar(numero) { // f -> Promesa
+    return new Promise(
+        (resolve, reject) => {
+            if (numero % 2 === 0) {
+                resolve(numero); /* return (then) */
+            } else {
+                reject(':( no es par'); /* throw (catch) */
+            }
+        }
+    )
+}
+function promesaElevarAlCuadrado(numero) {
+    return new Promise((res)=> res(Math.pow(numero, 2)));
+}
+promesaEsPar(5)
+    .then( // try
+        (data)=>{
+            console.log('DATA', data);
+            return promesaElevarAlCuadrado(data);
+        }
+     )
+    .then( // try
+        (data)=>{
+            console.log('DATA', data);
+            return promesaElevarAlCuadrado(data);
+        }
+     )
+    .then( // try
+        (data)=>{
+            console.log('DATA FINAL', data);
+            return promesaElevarAlCuadrado(data);
+        }
+     )
+    .catch( // catch
+        (error)=>{
+            console.error('ERROR', error);
+        }
+    )
+    .finally( // finally
+        ()=>{
+            console.log('FINALLY');
+        }
+    )
